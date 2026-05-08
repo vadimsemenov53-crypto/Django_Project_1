8e72b67a19077602e0f34bb2c4fed0a9d1b0aa44
Vadim Semenov <vadimsemenov53@gmail.com>
2026-05-08 14:52:47 +0300

    add feedback form in views

--- catalog/views.py
+++ catalog/views.py
@@ -7,4 +7,15 @@ def home(request):
 
 def contacts(request):
     """ Контроллер для страницы contacts.html """
-    return render(request, 'contacts.html')
+    success = False
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        success = True
+
+    return render(request, 'contacts.html', {
+            'success': success
+        })
