e63e3792964e3c94e6dbc143c76b959777b3f217
Vadim Semenov <vadimsemenov53@gmail.com>
2026-05-16 18:33:52 +0300

    add command add test data

---
+++ catalog/management/commands/add_test_info.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from catalog.models import Category, Product
+
+class Command(BaseCommand):
+
+    help = 'Загрузка тестовых данных. Перед применением удаляет все данные из БД.'
+
+    def handle(self, *args, **options):
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+
+        self.stdout.write(self.style.SUCCESS('Все данные удалены!'))
+
+        try:
+            call_command('loaddata', 'fixture_category.json')
+            call_command('loaddata', 'fixture_product.json')
+
+            self.stdout.write(self.style.SUCCESS('Фикстуры загружены!'))
+
+        except Exception as e:
+            self.stdout.write(self.style.WARNING(f'При загрузке данных произошла ошибка - {e}'))
