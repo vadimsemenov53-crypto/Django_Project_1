173ab05c238f50130dad6266aa0f16a1a344f45a
Vadim Semenov <vadimsemenov53@gmail.com>
2026-06-01 18:20:38 +0300

    add base_blog.html and main_menu.html & migrate for testing work

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from blogs.apps import BlogsConfig
+from blogs.views import BlogListView
 
 app_name = BlogsConfig.name
 
 urlpatterns = [
-
+    path('blog_list', BlogListView.as_view(), name='blog_list'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from blogs.models import BlogPost
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 
 # Create your views here.
+
+class BlogListView(ListView):
+    model = BlogPost
+    template_name = 'base_blog.html'
