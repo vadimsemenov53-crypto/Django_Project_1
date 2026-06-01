927b3b0878f4bd0e96390be7930dde7bc20eb57b
Vadim Semenov <vadimsemenov53@gmail.com>
2026-06-01 18:33:34 +0300

    registered in blogs/admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import BlogPost
 
 # Register your models here.
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'is_published', 'views_count',)
+    list_filter = ('created_at', 'is_published')
+    search_fields = ('title', 'content')
