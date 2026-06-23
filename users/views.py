1066f50217358679089ff2dc8d2c4cf6dcb44388
Vadim Semenov <vadimsemenov53@gmail.com>
2026-06-23 16:53:37 +0300

    add send mail & form valid in views.UserCreateView

--- users/views.py
+++ users/views.py
@@ -14,3 +14,38 @@ class UserCreateView(CreateView):
     model = User
     form_class = UserRegisterForm
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+
+        host = self.request.get_host()
+        url = f'http://{host}/users/email-confirm/{token}/'
+
+        send_mail(
+            subject='VADIM PRO Подтверждение почты',
+            message=f'Привет перейди по ссылке для подтверждения почты: {url}',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+
+        return super().form_valid(form)
+
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.token = None
+    user.save()
+
+    send_mail(
+        subject='Добро пожаловать в наш сервис',
+        message='Спасибо за регистрацию!',
+        from_email=EMAIL_HOST_USER,
+        recipient_list=[user.email]
+    )
+
+    return redirect(reverse('users:login'))
