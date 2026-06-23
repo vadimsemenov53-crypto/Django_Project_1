65c6f26733a6ba274d3f639ee7ee4cab590b0066
Vadim Semenov <vadimsemenov53@gmail.com>
2026-06-23 17:54:54 +0300

    add command create_admin for users & fixes AdminUser

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import User
-
-# Register your models here.
 
 @admin.register(User)
 class AdminUser(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_active',)
+    list_display = ('email', 'phone', 'is_active',)
+    list_filter = ('is_active',)
+    search_fields = ('email', 'phone',)
