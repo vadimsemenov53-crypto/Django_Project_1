98805fc7f34105ff20b487f9f2af6d70a34c24a3
Vadim Semenov <vadimsemenov53@gmail.com>
2026-06-28 14:21:41 +0300

    add user.owner right delete & change of its product

--- catalog/views.py
+++ catalog/views.py
@@ -48,7 +48,7 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
         if user.has_perm('catalog.can_unpublish_product'):
             return ProductModeratorForm
 
-        if user.is_authenticated:
+        if user == self.object.owner:
             return ProductForm
 
         raise PermissionDenied
